9113ea39ecd431776e73dbff9a5c55a83bf27df5
Rostyslav <rostyslav.kryvyy@gmail.com>
2013-08-29 19:05:02 +0300

    input formula in excel

--- IOExcel/src/InputExcel.java
+++ IOExcel/src/InputExcel.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -16,23 +15,45 @@ import org.apache.poi.ss.usermodel.Row;
 public class InputExcel {
 
 	public static void main(String args[]) {
-		new InputExcel().WExcel();
+		new InputExcel().createExcel();
 	}
 
-	public void WExcel() {
+	public void createExcel() {
 
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet("TableSheet");
+
+		inputDataInExcel(sheet);
+
+		inputFormulaInExcel(sheet);
+
+		try {
+			FileOutputStream out = new FileOutputStream(new File(
+					"Excel_from_java.xls"));
+			workbook.write(out);
+			out.close();
+			System.out.println("Excel written successfully...");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void inputDataInExcel(HSSFSheet sheet) {
+		
 		Map<String, Object[]> data = new HashMap<String, Object[]>();
 		data.put("1", new Object[] { "Emp No.", "Name", "Surname", "Age" });
 		data.put("2", new Object[] { 1d, "Aaaa", "Kkkkkk", 24d });
 		data.put("3", new Object[] { 2d, "Bbbb", "Mmmmm", 36d });
 		data.put("4", new Object[] { 3d, "Ccc", "Nnnnnnn", 30d });
 		data.put("5", new Object[] { 4d, "Ddddd", "Llll", 41d });
+		data.put("6", new Object[] { 5d, "Eeeeee", "Ooooo", 16d });
 
 		Set<String> keyset = data.keySet();
 		int rownum = 0;
-
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet("TableSheet");
 
 		for (String key : keyset) {
 			Row row = sheet.createRow(rownum++);
@@ -50,19 +71,17 @@ public class InputExcel {
 					cell.setCellValue((Double) obj);
 			}
 		}
+	}
 
-		try {
-			FileOutputStream out = new FileOutputStream(new File(
-					"Excel_from_java.xls"));
-			workbook.write(out);
-			out.close();
-			System.out.println("Excel written successfully...");
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void inputFormulaInExcel(HSSFSheet sheet) {
+		
+		Row header = sheet.createRow(10);
+		header.createCell(0).setCellValue("Celsius");
+		header.createCell(1).setCellValue("Fahrenheit");
+		
+		Row dataRow = sheet.createRow(11);
+	    dataRow.createCell(0).setCellValue(23d);
+	    dataRow.createCell(1).setCellFormula("A12*9/5+32");
 
 	}
 
