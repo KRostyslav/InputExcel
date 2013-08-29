7b4ccc4aa4001cbf5919d829915839f536513a91
Rostyslav <rostyslav.kryvyy@gmail.com>
2013-08-29 18:33:09 +0300

    update java class now write Set in excel

--- IOExcel/src/InputExcel.java
+++ IOExcel/src/InputExcel.java
@@ -1,33 +1,69 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 public class InputExcel {
-	
+
 	public static void main(String args[]) {
 		new InputExcel().WExcel();
 	}
 
 	public void WExcel() {
-		
-		HSSFWorkbook workbook = new HSSFWorkbook();		
+
+		Map<String, Object[]> data = new HashMap<String, Object[]>();
+		data.put("1", new Object[] { "Emp No.", "Name", "Surname", "Age" });
+		data.put("2", new Object[] { 1d, "Aaaa", "Kkkkkk", 24d });
+		data.put("3", new Object[] { 2d, "Bbbb", "Mmmmm", 36d });
+		data.put("4", new Object[] { 3d, "Ccc", "Nnnnnnn", 30d });
+		data.put("5", new Object[] { 4d, "Ddddd", "Llll", 41d });
+
+		Set<String> keyset = data.keySet();
+		int rownum = 0;
+
+		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("TableSheet");
-		HSSFRow row = sheet.createRow(0);
-		Cell cell = row.createCell(0);
-		cell.setCellValue((String) "Java One in cell A1");
+
+		for (String key : keyset) {
+			Row row = sheet.createRow(rownum++);
+			Object[] objArray = data.get(key);
+			int cellnum = 0;
+			for (Object obj : objArray) {
+				Cell cell = row.createCell(cellnum++);
+				if (obj instanceof Date)
+					cell.setCellValue((Date) obj);
+				else if (obj instanceof Boolean)
+					cell.setCellValue((Boolean) obj);
+				else if (obj instanceof String)
+					cell.setCellValue((String) obj);
+				else if (obj instanceof Double)
+					cell.setCellValue((Double) obj);
+			}
+		}
 
 		try {
-			FileOutputStream out = new FileOutputStream(new File("Excel_from_java.xls"));
+			FileOutputStream out = new FileOutputStream(new File(
+					"Excel_from_java.xls"));
 			workbook.write(out);
 			out.close();
+			System.out.println("Excel written successfully...");
 
-		} catch (Exception e) {
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 
 }
